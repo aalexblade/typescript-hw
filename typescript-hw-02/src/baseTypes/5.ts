2c746d15ab363c7efa7162f9c8c4bb19a2cae3d3
alex blade <aalexblade@gmail.com>
2025-07-03 23:53:13 +0300

    add resolve task 5

--- typescript-hw-02/src/baseTypes/5.ts
+++ typescript-hw-02/src/baseTypes/5.ts
@@ -3,5 +3,6 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let literal: 'enable' | 'disable';
+literal = 'enable';  // Це дозволено
+literal = 'disable'; 
