4cba931b7042635eb9e512250da2a49632689931
alex blade <aalexblade@gmail.com>
2025-07-22 22:50:20 +0300

    add new example

--- typescript-hw-02/src/generic/3.ts
+++ typescript-hw-02/src/generic/3.ts
@@ -3,6 +3,15 @@
   Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
 */
 
+// function merge (objA, objB) {
+//   return Object.assign(objA, objB);
+// }
+
+// export {}
+
+// =============================
+
+
 function merge (objA, objB) {
   return Object.assign(objA, objB);
 }
