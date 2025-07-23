3a8a88813861764550a2fb55c5f64a1d42ef8d51
alex blade <aalexblade@gmail.com>
2025-07-23 22:11:47 +0300

    add resolve

--- typescript-hw-02/src/generic/4.ts
+++ typescript-hw-02/src/generic/4.ts
@@ -2,6 +2,21 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
+// class Component {
+//   constructor (public props:T) {
+
+//   }
+// }
+
+// class Page extends Component {
+//   pageInfo () {
+//     console.log(this.props.title);
+//   }
+// }
+
+// export {};
+
+// =================================
 class Component {
   constructor (public props:T) {
 
