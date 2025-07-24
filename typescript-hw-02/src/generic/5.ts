b1ec5c06bf18896a96aaf0507321c8c1f592dccb
alex blade <aalexblade@gmail.com>
2025-07-24 22:51:31 +0300

    add resolve

--- typescript-hw-02/src/generic/5.ts
+++ typescript-hw-02/src/generic/5.ts
@@ -3,6 +3,14 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
+// interface KeyValuePair {
+//   key;
+//   value;
+// }
+
+// export {};
+// =============================
+
 interface KeyValuePair {
   key;
   value;
