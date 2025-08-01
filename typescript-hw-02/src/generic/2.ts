f27a59aa7b895854b58056b19342fdf840e9b919
alex blade <aalexblade@gmail.com>
2025-08-01 18:19:06 +0300

    add resolve task

--- typescript-hw-02/src/generic/2.ts
+++ typescript-hw-02/src/generic/2.ts
@@ -3,30 +3,6 @@
   Ваше завдання – використовувати Pick та generics для вказівки, що поля цих об'єктів належать AllType.
   Функція compare повинна повертати AllType.
 */
-// ====================================================
-// first resolve
-
-// type AllType = {
-//   name: string;
-//   position: number;
-//   color: string;
-//   weight: number;
-// };
-
-// function compare(top: AllType, bottom: AllType): AllType {
-//   return {
-//     name: top.name,
-//     color: top.color,
-//     position: bottom.position,
-//     weight: bottom.weight,
-//   };
-// }
-
-// export {};
-
-// ====================================================
-
-// second resolve
 
 type AllType = {
   name: string;
@@ -35,10 +11,7 @@ type AllType = {
   weight: number;
 };
 
-function compare<
-  T extends Pick<AllType, "name" | "color">,
-  U extends Pick<AllType, "position" | "weight">
->(top: T, bottom: U): AllType {
+function compare(top: AllType, bottom: AllType): AllType {
   return {
     name: top.name,
     color: top.color,
@@ -48,3 +21,4 @@ function compare<
 }
 
 export {};
+
