f8dd4aaf91fa598c4bf25ffbf284089bc276ce3b
alex blade <aalexblade@gmail.com>
2025-08-04 21:55:51 +0300

    add resolve ts1

--- typescript-hw-02/src/generic/1.ts
+++ typescript-hw-02/src/generic/1.ts
@@ -3,15 +3,25 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T>(): Promise<T> {
+// function getPromise<T>(): Promise<T> {
+//   return new Promise((resolve) => {
+//     resolve(["Text", 50] as T);
+//   });
+// }
+
+// getPromise<(string | number)[]>().then((data) => {
+//   console.log(data);
+// });
+
+// export {};
+
+function getPromise<T> ():Promise<T> {
   return new Promise((resolve) => {
-    resolve(["Text", 50] as T);
+    resolve(['Text', 50]as T);
   });
 }
 
-getPromise<(string | number)[]>().then((data) => {
+getPromise<(string|number)[]>()
+.then((data) => {
   console.log(data);
 });
-
-export {};
-
