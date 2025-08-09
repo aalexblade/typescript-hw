916dfbd7b4b2ef370df0a0833785ec57019b3d0d
alex blade <aalexblade@gmail.com>
2025-08-09 21:16:26 +0300

    add resolve task 5

--- typescript-hw-02/src/generic/5.ts
+++ typescript-hw-02/src/generic/5.ts
@@ -3,11 +3,27 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+interface KeyValuePair<K, V> {
+  key: K;
+  value: V;
 }
 
+const pair1: KeyValuePair<string, number> = {
+  key: "age",
+  value: 30,
+};
+
+const pair2: KeyValuePair<number, string> = {
+  key: 1,
+  value: "one",
+};
+
+const pair3: KeyValuePair<boolean, string>[] = {
+  key: true,
+  value: ["bob", "alice", "charlie"],
+};
+
+console.log(pair1);
+console.log(pair2);
+console.log(pair3);
 export {};
-
-
