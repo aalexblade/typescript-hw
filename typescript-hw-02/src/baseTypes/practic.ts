b02409b4696ca859fe3afa6d091960fca5a0e13b
alex blade <aalexblade@gmail.com>
2025-08-20 21:58:58 +0300

    add task practic

---
+++ typescript-hw-02/src/baseTypes/practic.ts
@@ -0,0 +1,22 @@
+interface User {
+  username: string;
+  age: number;
+  isActive: boolean;
+}
+
+interface Admin extends User {
+  permissions: string[];
+}
+
+let users: User[] = [
+  { username: "Vlad", age: 22, isActive: true },
+  { username: "Anna", age: 19, isActive: false },
+];
+
+let superAdmin: Admin = {
+  username: "Oleh",
+  age: 30,
+  isActive: true,
+  permissions: ["manage-users", "edit-products"],
+};
+export {};
