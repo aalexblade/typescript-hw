18ffa52569aa2fe4fabc1e5265d560207a7364a1
alex blade <aalexblade@gmail.com>
2025-08-24 21:19:21 +0300

    add practics

--- typescript-hw-02/index.ts
+++ typescript-hw-02/index.ts
@@ -44,11 +44,52 @@ console.log(greet("Vlad"));
 
 // Якщо об’єкт використовується багато разів, зручно винести його в тип:
 
-type User = {
-  name: string;
-  age: number;
-};
+// type User = {
+//   name: string;
+//   age: number;
+// };
 
-let u1: User = { name: "Vlad", age: 22 };
+// let u1: User = { name: "Vlad", age: 22 };
 
 // або через інтерфейс:
+
+// interface User{
+//   name:string;
+//   age: number;
+// }
+
+// let u2:User={name: 'Hanna', age: 25};
+
+// ===========================================
+// Union (декілька можливих типів)
+
+// function identity<T>(value: T): T {
+//   return value;
+// }
+
+// let num = identity<number>(42);
+// let str = identity<string>("Hello");
+
+
+// ============================================
+// Типізація класів
+
+// class Person {
+//   name:string;
+//   age:number;
+
+//   constructor(name:string, age:number){
+//     this.name=name;
+//     this.age=age
+//   }
+// }
+
+//   greet(): void {
+//     console.log(`Hi, my name is ${this.name}`);
+//   }
+// }
+
+// const p1 = new Person('Cook', 32);
+// p1.greet(); // Hi, my name is Cook
+
+// ============================================
