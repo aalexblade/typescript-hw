a6cc1ce061e098dae3511ee76ae7f304ba5ee080
alex blade <aalexblade@gmail.com>
2025-09-06 20:26:53 +0300

    add commit

--- typescript-hw-02/src/baseTypes/9.ts
+++ typescript-hw-02/src/baseTypes/9.ts
@@ -1,48 +1,15 @@
-// /*
-//   Створіть новий тип даних, який підходить для цих двох об'єктів.
-// */
-
-// // interface Page {
-// //   title: string;
-// //   likes: number;
-// //   accounts: string[];
-// //   status: "open" | "close";
-// //   details?:  {
-// //     createAt: Date;
-// //     updateAt: Date;
-// //   };
-// // }
-
-// // const page1: Page = {
-// //   title: "The awesome page",
-// //   likes: 100,
-// //   accounts: ["Max", "Anton", "Nikita"],
-// //   status: "open",
-// //   details: {
-// //     createAt: new Date("2021-01-01"),
-// //     updateAt: new Date("2021-05-01"),
-// //   },
-// // };
-
-// // const page2: Page = {
-// //   title: "Python or Js",
-// //   likes: 5,
-// //   accounts: ["Alex"],
-// //   status: "close",
-// // };
-
-// // export {};
-
-// // --- Початок файлу ---
+/*
+  Створіть новий тип даних, який підходить для цих двох об'єктів.
+*/
 
 // interface Page {
 //   title: string;
-//   readonly likes: number;
-//   readonly accounts: ReadonlyArray<string>;
-//   status: "open" | "closed";
-//   details?: {
-//     readonly createdAt: Date;
-//     readonly updatedAt: Date;
+//   likes: number;
+//   accounts: string[];
+//   status: "open" | "close";
+//   details?:  {
+//     createAt: Date;
+//     updateAt: Date;
 //   };
 // }
 
@@ -52,8 +19,8 @@
 //   accounts: ["Max", "Anton", "Nikita"],
 //   status: "open",
 //   details: {
-//     createdAt: new Date("2021-01-01T00:00:00Z"),
-//     updatedAt: new Date("2021-05-01T12:00:00Z"),
+//     createAt: new Date("2021-01-01"),
+//     updateAt: new Date("2021-05-01"),
 //   },
 // };
 
@@ -61,10 +28,43 @@
 //   title: "Python or Js",
 //   likes: 5,
 //   accounts: ["Alex"],
-//   status: "closed",
+//   status: "close",
 // };
 
-// export {}; // Цей рядок може бути причиною, якщо ви очікуєте глобальної доступності
-
-// // --- Кінець файлу ---
 // export {};
+
+// --- Початок файлу ---
+
+interface Page {
+  title: string;
+  readonly likes: number;
+  readonly accounts: ReadonlyArray<string>;
+  status: "open" | "closed";
+  details?: {
+    readonly createdAt: Date;
+    readonly updatedAt: Date;
+  };
+}
+
+const page1: Page = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createdAt: new Date("2021-01-01T00:00:00Z"),
+    updatedAt: new Date("2021-05-01T12:00:00Z"),
+  },
+};
+
+const page2: Page = {
+  title: "Python or Js",
+  likes: 5,
+  accounts: ["Alex"],
+  status: "closed",
+};
+
+export {}; // Цей рядок може бути причиною, якщо ви очікуєте глобальної доступності
+
+// --- Кінець файлу ---
+export {};
