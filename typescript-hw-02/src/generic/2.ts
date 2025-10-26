ed23e0f4c5d439e64ee2e6b966e693e3cd0544e3
alex blade <aalexblade@gmail.com>
2025-10-26 20:37:42 +0200

    add commit

--- typescript-hw-02/src/generic/2.ts
+++ typescript-hw-02/src/generic/2.ts
@@ -4,22 +4,22 @@
   Функція compare повинна повертати AllType.
 */
 
-type AllType = {
-  name: string;
-  position: number;
-  color: string;
-  weight: number;
-};
+// type AllType = {
+//   name: string;
+//   position: number;
+//   color: string;
+//   weight: number;
+// };
 
-function compare<
-  T extends Pick<AllType, "name" | "color">,
-  U extends Pick<AllType, "position" | "weight">
->(top: T, bottom: U): AllType {
-  return {
-    name: top.name,
-    color: top.color,
-    position: bottom.position,
-    weight: bottom.weight,
-  };
-}
-export {};
+// function compare<
+//   T extends Pick<AllType, "name" | "color">,
+//   U extends Pick<AllType, "position" | "weight">
+// >(top: T, bottom: U): AllType {
+//   return {
+//     name: top.name,
+//     color: top.color,
+//     position: bottom.position,
+//     weight: bottom.weight,
+//   };
+// }
+// export {};
