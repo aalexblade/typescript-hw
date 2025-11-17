1a4b360f7366bca478db2129b363b5be21852a67
alex blade <aalexblade@gmail.com>
2025-11-17 20:42:44 +0200

    added S.O.L.I.D and Open/closed principle examples to the test.ts file

--- typescript-hw-03/src/test.ts
+++ typescript-hw-03/src/test.ts
@@ -1,5 +1,7 @@
 // Інкапсуляція в об'єктно-орієнтованому програмуванні (ООП)
 
+import { server } from '../node_modules/typescript/lib/typescript';
+
 class MyClass {
   private protectedMethod() {
     return 'Something';
@@ -43,13 +45,15 @@ class Animal {
 }
 
 class Cat extends Animal {
-  public say() { // перевизначення методу say() для Cat
+  public say() {
+    // перевизначення методу say() для Cat
     console.log('Meow');
   }
 }
 
 class Dog extends Animal {
-  public say() { // перевизначення методу say() для Dog
+  public say() {
+    // перевизначення методу say() для Dog
     console.log('Woof');
   }
 }
@@ -63,18 +67,119 @@ animal.say(); // Виводить "Woof"
 // =================================Абстракція
 
 class classA {
-  private process1 () {
+  private process1() {
     return 1;
   }
-  private process2 () {
+  private process2() {
     return 2;
   }
-  private process3 () {
+  private process3() {
     return 3;
   }
 
-  public superProcess () {
+  public superProcess() {
     return this.process1() + this.process2() + this.process3();
   }
 }
 
+// ==============================Принципи ООП (S.O.L.I.D)
+
+// Невірно: клас робить занадто багато речей
+class User {
+  public name: string;
+  public email: string;
+
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+
+  public save(): void {
+    // Логіка для збереження користувача в базі даних
+  }
+
+  public sendEmail(message: string): void {
+    // Логіка для відправки електронної пошти користувачу
+  }
+}
+
+// Вірно: кожен клас виконує тільки одну задачу
+
+class User {
+  public name: string;
+  public email: string;
+
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
+
+  public save(): void {
+    // Логіка для збереження користувача в базі даних
+  }
+}
+
+class EmailService {
+  public sendEmail(user: User, message: string): void {
+    // Логіка для відправки електронної пошти користувачу
+  }
+}
+
+// ==========================OCP (Open/closed principle) - Принцип відкритості/закритості
+
+class Rectangle {
+  public width: number;
+  public height: number;
+
+  constructor(width: number, height: number) {
+    this.width = width;
+    this.height = height;
+  }
+}
+
+class AreaCalculator {
+  public calculate(rectangle: Rectangle): number {
+    return rectangle.width * rectangle.height;
+  }
+}
+
+
+// ============================= Ми могли б змінити клас AreaCalculator, щоб він міг обробляти кола, але це призведе до зміни вже наявного коду, що порушує принцип відкритості/закритості.
+
+// використовувати абстракцію та поліморфізм, щоб задовольнити цей принцип:
+
+interface Shape {
+    calculateArea(): number;
+}
+
+class Rectangle implements Shape {
+    public width: number;
+    public height: number;
+    
+    constructor(width: number, height: number) {
+        this.width = width;
+        this.height = height;
+    }
+
+    calculateArea(): number {
+        return this.width * this.height;
+    }
+}
+
+class Circle implements Shape {
+    public radius: number;
+    
+    constructor(radius: number) {
+        this.radius = radius;
+    }
+
+    calculateArea(): number {
+        return Math.PI * Math.pow(this.radius, 2);
+    }
+}
+
+class AreaCalculator {
+    public calculate(shape: Shape): number {
+        return shape.calculateArea();
+    }
+}
