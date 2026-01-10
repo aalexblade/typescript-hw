9a018d19687195976b7367d25e45b1647d22987a
alex blade <aalexblade@gmail.com>
2026-01-10 19:23:21 +0200

    add solution

--- typescript-hw-03/src/2.ts
+++ typescript-hw-03/src/2.ts
@@ -14,29 +14,30 @@
 
 */
 
-// class Employee {
-//   // Заповніть модифікатори доступу
-//   public name: string;
-//   private department: string;
-//   protected salary: number;
+class Employee {
+  // Заповніть модифікатори доступу
+  public name: string;
+  private department: string;
+  protected salary: number;
 
-//   constructor(name: string, department: string, salary: number) {
-//     this.name = name;
-//     this.department = department;
-//     this.salary = salary;
-//   }
+  constructor(name: string, department: string, salary: number) {
+    this.name = name;
+    this.department = department;
+    this.salary = salary;
+  }
 
-//   getEmployeeDetails() {
-//     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
-//   }
-// }
+  getEmployeeDetails() {
+    return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
+  }
+}
 
-// class Manager extends Employee {
-//   // Реалізуйте конструктор та збільшіть salary на 10000
-//   constructor(name: string, department: string, salary: number) {
-//     super(name, department, salary);
-//     this.salary += 10000;
-//   }
-// }
+class Manager extends Employee {
+  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+    this.salary += 10000;
+  }
+}
 
-// export {};
+
+export {};
