28ed721de331a36b96e274a6078a02a98993ca5b
alex blade <aalexblade@gmail.com>
2026-01-19 22:41:24 +0200

    add commit

--- typescript-hw-03/src/test.ts
+++ typescript-hw-03/src/test.ts
@@ -88,23 +88,23 @@ class classA {
 // ==============================Принципи ООП (S.O.L.I.D)
 
 // Невірно: клас робить занадто багато речей
-// class User {
-//   public name: string;
-//   public email: string;
+class User {
+  public name: string;
+  public email: string;
 
-//   constructor(name: string, email: string) {
-//     this.name = name;
-//     this.email = email;
-//   }
+  constructor(name: string, email: string) {
+    this.name = name;
+    this.email = email;
+  }
 
-//   public save(): void {
-//     // Логіка для збереження користувача в базі даних
-//   }
+  public save(): void {
+    // Логіка для збереження користувача в базі даних
+  }
 
-//   public sendEmail(message: string): void {
-//     // Логіка для відправки електронної пошти користувачу
-//   }
-// }
+  public sendEmail(message: string): void {
+    // Логіка для відправки електронної пошти користувачу
+  }
+}
 
 // Вірно: кожен клас виконує тільки одну задачу
 
