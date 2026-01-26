f7ee79914cc600d531e5aec99088d5e46ecbbd57
alex blade <aalexblade@gmail.com>
2026-01-26 15:56:43 +0200

    add commit

--- typescript-hw-03/src/test.ts
+++ typescript-hw-03/src/test.ts
@@ -69,21 +69,21 @@ animal.say(); // Виводить "Woof"
 
 // =================================Абстракція
 
-class classA {
-  private process1() {
-    return 1;
-  }
-  private process2() {
-    return 2;
-  }
-  private process3() {
-    return 3;
-  }
+// class classA {
+//   private process1() {
+//     return 1;
+//   }
+//   private process2() {
+//     return 2;
+//   }
+//   private process3() {
+//     return 3;
+//   }
 
-  public superProcess() {
-    return this.process1() + this.process2() + this.process3();
-  }
-}
+//   public superProcess() {
+//     return this.process1() + this.process2() + this.process3();
+//   }
+// }
 
 // ==============================Принципи ООП (S.O.L.I.D)
 
