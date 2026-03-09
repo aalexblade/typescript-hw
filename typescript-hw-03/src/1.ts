dfdb4df14379c229024235b184495f8d4d1732de
alex blade <aalexblade@gmail.com>
2026-03-09 21:00:50 +0200

    adde commit

--- typescript-hw-03/src/1.ts
+++ typescript-hw-03/src/1.ts
@@ -4,31 +4,31 @@
   напишіть скорочену ініціалізацію.
 */
 
-class Student {
-  public name: string;
-  public age: number;
-  public grade: string;
-
-  constructor(name: string, age: number, grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
-  }
-}
-
-export {};
-
-// ====================================================
-
 // class Student {
-//   // TypeScript автоматично створить і проініціалізує властивості
-//   constructor(public name: string, public age: number, public grade: string) {
-//     // Тіло конструктора залишається пустим
+//   public name: string;
+//   public age: number;
+//   public grade: string;
+
+//   constructor(name: string, age: number, grade: string) {
+//     this.name = name;
+//     this.age = age;
+//     this.grade = grade;
 //   }
 // }
 
-// const student1 = new Student('Ivan', 20, 'A');
+// export {};
 
-// console.log(student1.name);
-// console.log(student1.age);
-// console.log(student1.grade);
+// ====================================================
+
+class Student {
+  // TypeScript автоматично створить і проініціалізує властивості
+  constructor(public name: string, public age: number, public grade: string) {
+    // Тіло конструктора залишається пустим
+  }
+}
+
+const student1 = new Student('Ivan', 20, 'A');
+
+console.log(student1.name);
+console.log(student1.age);
+console.log(student1.grade);
